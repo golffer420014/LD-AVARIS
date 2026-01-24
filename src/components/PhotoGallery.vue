1082948ec9dd957e20019780926d1dcf61b1d3d3
suriya <isupermax.11@gmail.com>
2026-01-25 00:45:49 +0700

    update

--- src/components/PhotoGallery.vue
+++ src/components/PhotoGallery.vue
@@ -36,7 +36,7 @@ onUnmounted(() => {
             :data-pswp-height="image.height" target="_blank" rel="noreferrer"
             class="aspect-square overflow-hidden group relative bg-gray-100">
             <img :src="image.src" :alt="image.alt || 'Gallery image'"
-                class="border border-black/70 w-full h-full object-cover transition-transform duration-500" />
+                class="border border-black/60 w-full h-full object-cover transition-transform duration-500" />
             <div
                 class="absolute inset-0 bg-black/5 opacity-0 group-hover:opacity-100 transition-opacity flex items-center justify-center">
                 <!-- <span class="text-white text-xs tracking-widest uppercase opacity-0 group-hover:opacity-100 transition-opacity delay-100">View</span> -->
